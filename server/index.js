6ec1ac4a8fba0db49dd78af3d5af98360952bab0
ChangJoo Park <pcjpcj2@gmail.com>
2019-02-05 22:56:59 +0900

    Update layout using ejs-mate

--- server/index.js
+++ server/index.js
@@ -3,21 +3,25 @@ const express = require('express')
 const webpack = require('webpack')
 const webpackMiddleware = require('webpack-dev-middleware')
 const webpackConfig = require('../webpack.config')
-
+const viewEngine = require('ejs-mate')
 const app = express()
 const port = 9000
 const publicPath = path.join(__dirname, '../', 'public')
 const viewPath = path.join(__dirname, '../client/', 'views')
-const viewEngine = 'ejs'
 
 module.exports = function() {
   app.set('views', viewPath)
-  app.set('view engine', viewEngine)
+  app.engine('ejs', viewEngine)
+  app.set('view engine', 'ejs')
 
   app.use(express.static(publicPath))
   app.use(webpackMiddleware(webpack(webpackConfig)))
 
   const pages = [
+    {
+      path: '/',
+      title: 'Home'
+    },
     {
       path: '/hello',
       title: 'Hello'
@@ -37,7 +41,13 @@ module.exports = function() {
   ]
 
   app.get('/', (req, res) => {
-    res.redirect(pages[0].path)
+    res.render('index.ejs', {
+      pages,
+      currentPage: {
+        path: '/',
+        title: 'Home'
+      }
+    })
   })
 
   app.get('/uptime', (req, res, next) => {
