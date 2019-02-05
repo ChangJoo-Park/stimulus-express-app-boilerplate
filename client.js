59aa77359adddfc6ba4414243ad34ba6a52010a0
ChangJoo Park <pcjpcj2@gmail.com>
2019-02-05 21:58:20 +0900

    move client code to ./client, move server code to ./server

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-import "@stimulus/polyfills"
+//import "@stimulus/polyfills"
 import { Application } from "@stimulus/core"
 import { definitionsFromContext } from "@stimulus/webpack-helpers"
 import Turbolinks from "turbolinks"
@@ -6,5 +6,5 @@ import Turbolinks from "turbolinks"
 Turbolinks.start()
 
 const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
+const context = require.context("./client/controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
